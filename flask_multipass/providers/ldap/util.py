e9f318dc0cc25bad8bd510d7b82e17701d47c272
Indico Team <indico-team@cern.ch>
2017-03-21 12:30:03 +0100

    Bump copyright year

--- flask_multipass/providers/ldap/util.py
+++ flask_multipass/providers/ldap/util.py
@@ -1,5 +1,5 @@
 # This file is part of Flask-Multipass.
-# Copyright (C) 2015 CERN
+# Copyright (C) 2015 - 2017 CERN
 #
 # Flask-Multipass is free software; you can redistribute it
 # and/or modify it under the terms of the Revised BSD License.
