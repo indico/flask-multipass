ab8a71cfbb2b12c263cd4540bdb19baabc2da258
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-05-30 11:33:44 +0200

    Shibboleth: optionally get auth data from headers

--- flask_multipass/providers/shibboleth.py
+++ flask_multipass/providers/shibboleth.py
@@ -22,6 +22,11 @@ def _to_unicode(s):
     return s.decode('utf-8') if PY2 and isinstance(s, str) else s
 
 
+def _lower_keys(iter_):
+    for k, v in iter_:
+        yield k.lower(), v
+
+
 class ShibbolethAuthProvider(AuthProvider):
     """Provides authentication using Shibboleth.
 
@@ -33,9 +38,12 @@ class ShibbolethAuthProvider(AuthProvider):
 
     def __init__(self, *args, **kwargs):
         super(ShibbolethAuthProvider, self).__init__(*args, **kwargs)
-        self.settings.setdefault('attrs_prefix', 'ADFS_')
+        # convert everything to lowercase (headers/WSGI vars are case-insensitive)
+        self.attrs_prefix = self.settings.setdefault('attrs_prefix', 'ADFS_').lower()
+        self.attrs = [attr.lower() for attr in self.settings.get('attrs')]
         if not self.settings.get('callback_uri'):
             raise MultipassException("`callback_uri` must be specified in the provider settings", provider=self)
+        self.from_headers = self.settings.get('from_headers', False)
         self.shibboleth_endpoint = '_flaskmultipass_shibboleth_' + self.name
         current_app.add_url_rule(self.settings['callback_uri'], self.shibboleth_endpoint,
                                  self._shibboleth_callback, methods=('GET', 'POST'))
@@ -50,8 +58,13 @@ class ShibbolethAuthProvider(AuthProvider):
 
     @login_view
     def _shibboleth_callback(self):
-        attributes = {k: _to_unicode(v)
-                      for k, v in iteritems(request.environ) if k.startswith(self.settings['attrs_prefix'])}
+        mapping = _lower_keys(iteritems(request.headers if self.from_headers else request.environ))
+        # get all attrs in the 'attrs' list, if empty use 'attrs_prefix'
+        if self.attrs is None:
+            attributes = {k: _to_unicode(v) for k, v in mapping if k.startswith(self.attrs_prefix)}
+        else:
+            attributes = {k: _to_unicode(v) for k, v in mapping if k in self.attrs}
+
         if not attributes:
             raise AuthenticationFailed("No valid data received", provider=self)
         return self.multipass.handle_auth_success(AuthInfo(self, **attributes))
@@ -72,10 +85,12 @@ class ShibbolethIdentityProvider(IdentityProvider):
 
     def __init__(self, *args, **kwargs):
         super(ShibbolethIdentityProvider, self).__init__(*args, **kwargs)
-        self.settings.setdefault('identifier_field', 'ADFS_LOGIN')
+        # make headers/vars case-insensitive
+        self.id_field = self.settings.setdefault('identifier_field', 'ADFS_LOGIN').lower()
+        self.settings['mapping'] = {k: v.lower() for k, v in self.settings['mapping'].items()}
 
     def get_identity_from_auth(self, auth_info):
-        identifier = auth_info.data.get(self.settings['identifier_field'])
+        identifier = auth_info.data.get(self.id_field)
         if not identifier:
             raise IdentityRetrievalFailed('Identifier missing in shibboleth response', provider=self)
         return IdentityInfo(self, identifier=identifier, **auth_info.data)
