3d5c99f0b375a77c12a44c15cd7a45229c279525
Adrian Moennich <adrian.moennich@cern.ch>
2018-06-04 11:10:02 +0200

    Remove Python 3.3 support/tests

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
