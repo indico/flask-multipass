9cf86c502f99812a745a0875e9659e0dcc9694cf
Adrian Moennich <adrian.moennich@cern.ch>
2019-11-05 14:41:28 +0100

    Drop python 3.4, 3.5 & support 3.7, 3.8 instead

--- setup.py
+++ setup.py
@@ -47,9 +47,9 @@ setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     entry_points={
         'flask_multipass.auth_providers': {
