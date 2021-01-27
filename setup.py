bb9eec5c7d8e1297a1eec60416a8aae1626de65f
Adrian Moennich <adrian.moennich@cern.ch>
2021-01-27 15:24:09 +0100

    Explicitly support Python 3.9

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     entry_points={
         'flask_multipass.auth_providers': {
