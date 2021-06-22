02f2389294b373ae5f785a76dd9f90bca486e7e0
Adrian Moennich <adrian.moennich@cern.ch>
2021-06-22 14:31:09 +0200

    Bump version to 0.4.1

--- flask_multipass/__init__.py
+++ flask_multipass/__init__.py
@@ -13,7 +13,7 @@ from .group import Group
 from .identity import IdentityProvider
 
 
-__version__ = '0.4'
+__version__ = '0.4.1'
 __all__ = ('Multipass', 'AuthProvider', 'IdentityProvider', 'AuthInfo', 'IdentityInfo', 'Group', 'MultipassException',
            'AuthenticationFailed', 'IdentityRetrievalFailed', 'GroupRetrievalFailed', 'NoSuchUser',
            'InvalidCredentials')
