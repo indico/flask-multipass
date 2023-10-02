4d21c346fa18432dcb20ed1b5954315465e92360
Adrian Moennich <adrian.moennich@cern.ch>
2023-10-02 17:44:48 +0200

    Modernize release CI + enable dependabot updates

--- .github/utils/check_version.py
+++ .github/utils/check_version.py
@@ -1,15 +1,15 @@
 import os
 import sys
+from importlib.util import find_spec
 
-from setuptools.config import StaticModule
+from setuptools.config.expand import StaticModule
 
 
+package = 'flask_multipass'
 sys.path.insert(0, os.getcwd())
-version = StaticModule('flask_multipass').__version__
+version = StaticModule(package, find_spec(package)).__version__
 tag_version = sys.argv[1]
 
 if tag_version != version:
-    print(
-        f'::error::Tag version {tag_version} does not match package version {version}'
-    )
+    print(f'::error::Tag version {tag_version} does not match package version {version}')
     sys.exit(1)
