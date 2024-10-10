6c76ea73a2a288ca7e822308ca08cc32496bbbb1
Alejandro Avilés <alejandro.aviles@un.org>
2024-10-10 15:44:08 +0200

    Pass username/identifier to InvalidCredentials

--- flask_multipass/exceptions.py
+++ flask_multipass/exceptions.py
@@ -32,8 +32,10 @@ class NoSuchUser(AuthenticationFailed):
 class InvalidCredentials(AuthenticationFailed):
     """Indicates a failure to authenticate using the given credentials."""
 
-    def __init__(self, details=None, provider=None):
+    def __init__(self, details=None, provider=None, username=None, identifier=None):
         AuthenticationFailed.__init__(self, 'Invalid credentials', details=details, provider=provider)
+        self.username = username
+        self.identifier = identifier
 
 
 class IdentityRetrievalFailed(MultipassException):
